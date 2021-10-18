9f9b1b7a001f288035703be07634bfe6f9eb2e0f
Ricardo Glez Leal <ricardoglr664@gmail.com>
2021-10-18 13:55:07 +0000

    Prácticas Sistemas Operativos

--- numarch.c
+++ numarch.c
@@ -7,12 +7,14 @@ int main() {
     pipe(fd);
     int pid = fork(); 
     if(pid==0){
-        //close(0);
-        dup2(1,fd[1]);
-        execlp("wc","wc","-1",NULL);
+        close(0);
+        dup(fd[0]);
+        close(fd[1]);
+        execlp("wc","wc","-l",NULL);
     } else {
-        //close(1);
-        dup2(0,fd[0]);
+        close(1);
+        dup(fd[1]);
+        close(fd[0]);
         execlp("ls","ls",NULL);
     }
     return 0;
