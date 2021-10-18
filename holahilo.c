40d7fa5fdadb496b462367893c1a0a8713523d69
Ricardo Glez Leal <ricardoglr664@gmail.com>
2021-10-18 12:18:28 +0000

    Introduccion a hilos

--- holahilo.c
+++ holahilo.c
@@ -2,21 +2,30 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define NUM_TRHREADS 20
+#define NUM_TRHREADS 100
+int saldo;
 
 void *printHola(void *arg){
-    printf("hola desde un hilo\n");
-    while(1) {
+    //printf("hola desde un hilo\n");
+    /*while(1) {
         printf("Trabajando\n");
         sleep(1);
-    }
+    }*/
+    int saldolocal = saldo;
+    saldolocal += 100;
+    saldo = saldolocal;
     pthread_exit(NULL);
 }
 
 int main(){
     pthread_t threads[NUM_TRHREADS];
-for (int i = 0; i < NUM_TRHREADS; i++) {
-    pthread_create(&threads[i],NULL,printHola, NULL); 
-}
+    saldo = 0;
+    for (int i = 0; i < NUM_TRHREADS; i++) {
+        pthread_create(&threads[i],NULL,printHola, NULL); 
+    }
+    for (int i = 0; i < NUM_TRHREADS; i++) {
+        pthread_join(threads[i],NULL); 
+    }
+    printf("El valor del saldo es %d \n", saldo);
     pthread_exit(NULL); 
 }
